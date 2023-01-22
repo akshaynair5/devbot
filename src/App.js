9dac1d98b0081a8c34d9e63bc01b8eb70c55bcb3
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2023-01-22 17:55:06 +0400

    1.0

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import './App.css';
 import sendimg from './assets/send.svg'
+import axios from 'axios'
+import {useState} from 'react'
 
 function App() {
+  const [prompt,setPrompt] = useState("") 
+  const [response,setRes] = useState("")
+  const [userChat,setUc] = useState([])
+  const [botChat,setBc] = useState([])
+  const onSubmit = async (e) =>{
+    e.preventDefault()
+    const temp = userChat.concat({chat:prompt})
+    setUc(temp)
+    await axios.post('http://localhost:3001/',{
+      prompt:prompt
+    })
+      .then(function(response){
+        console.log(response)
+        setRes(response)
+        const temp = userChat.concat({chat:response.data.bot})
+        setUc(temp)
+      })
+  }
   return (
     <div id="App">
       <div id="chat_container">
-
-        
+        {
+          userChat.map((chat)=>{
+            <div className='chat'>
+              <p>{chat}</p>
+            </div>
+          })
+        }
         <form>
-          <textarea name="prompt" rows="1" cols="1" ></textarea>
-          <button type="submit"><img src={sendimg}></img></button>
+          <textarea name="prompt" rows="1" cols="1" onChange={(e)=>setPrompt(e.target.value)}></textarea>
+          <button type="submit" onClick={(e)=>onSubmit(e)}><img src={sendimg}></img></button>
         </form>
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
